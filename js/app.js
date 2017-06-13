008e1f84d36aaa1ae8da94a2215ba9729b49b5dd
Marek Lach <marek.lach91@gmail.com>
2017-06-13 21:31:15 +0200

    ready

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(function(){
     
     
+//    hamburger menu
+    
     $('#x-menu').click(function(){
 		$(this).toggleClass('open');
     });
@@ -10,10 +12,17 @@ $(function(){
 		$('.hide').toggleClass('show');
     });
     
+//    prevent default
+       
+    $('ul li').on('click', function(e){
+    e.preventDefault(); 
+    })
+    
+// API  
     
     const currentState = {
         sortColumn: 'id',
-        sortOrder: 'asc',  // AKTUALNY STAN
+        sortOrder: 'asc',  // aktualny stan
         filter: "",
         page: 1,
         page_size: 20
@@ -39,24 +48,24 @@ $(function(){
     )
     const buildList = (items) => items.map(buildItem) // iterowanie po headerach  
 //    items.map(function(item) => { return buildItem(item)})
-    const buildHeaderItem = (item) => {
+    
+    const buildHeaderItem = (item) => { 
         let sort = 'asc'; // domysle 
-        
         if(item.sortColumn === currentState.sortColumn){
-            sort = currentState.sortOrder === sort ? 'desc' : 'asc'  // skrócona wersja if i esle  
+            sort = currentState.sortOrder === sort ? 'desc' : 'asc'  // skrócenie if i esle  
             currentState.sortOrder = sort  
         }
         
-        
         return `<th>
-            <a class="js-sort" href="${apiUrl}?sort_column=${item.sortColumn}&sort_order=${sort}&filter=${currentState.filter}">${item.name}</a>
+            <a data-column="${item.sortColumn}" class="js-sort" href="${apiUrl}?sort_column=${item.sortColumn}&sort_order=${sort}&filter=${currentState.filter}">${item.name}</a>
                </th>`
+        
     }
     
     const buildHeader = () => sortingMethods.map(buildHeaderItem)
     
     
-
+// ajax question
     
     const getData = (url) => {
 
@@ -65,7 +74,7 @@ $(function(){
             url: url,
             
             success: function(data, status, xhr){
-                $('.js-results-list').empty().append(buildList(data))
+                $('.js-results-list').empty().append(buildList(data))  // czyszczenie 
                 $('.js-results-header').empty().append(buildHeader())
             }, 
 
@@ -74,16 +83,19 @@ $(function(){
             }
         })
     }
-    
     getData(generateUrl());
         
     $(document).on('click', '.js-sort', function(e){ // ominięcie przeładowania do nowej strony przez onlcick. Przeniesienie nazwy klasy do funkcji 
         e.preventDefault();
+        currentState.sortColumn = $(this).data('column')
         const url = $(this).attr("href")
+       
         getData(url);  
     })
     
-
+    
+//    filter
+    
     $('.js-search-form').on('submit', function(e){
         e.preventDefault();
         currentState.filter = $('.js-search-form-filter').val()
@@ -91,27 +103,26 @@ $(function(){
         getData(generateUrl());   
     })
     
+//    prev button
+    
     $('.js-prev').on('click', function(e){
         e.preventDefault();
         if( currentState.page > 1){
-            currentState.page--;
-            
+            currentState.page--;   
         }
-        getData(generateUrl());
-        
-        
+        getData(generateUrl());  
     })
    
+  //    next button  
     
      $('.js-next').on('click', function(e){
         e.preventDefault(); 
         currentState.page++; 
         getData(generateUrl());   
      })
+ 
     
-    $('ul li').on('click', function(e){
-    e.preventDefault(); 
-    })
+//    counting page show 
     
  $(function(){ 
     let i = 1; 
